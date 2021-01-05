497b5b4b77b20b3309a63bee1dd77984a5965f73
St4rd_l0rd <devesh224.cavisson@gmail.com>
2021-01-05 21:24:40 +0530

    files updation

---
+++ app.js
@@ -0,0 +1,16 @@
+var price= document.querySelector("#price_text");
+var currency = ["USD", "GBP" ,"EUR"];
+var data = [];
+var XHR = new XMLHttpRequest();
+XHR.onreadystatechange = function(){
+                                    if(XHR.readyState == 4 && XHR.status == 200)
+			                       {
+			                       // data
+			                       data = Object.values(JSON.parse(XHR.responseText).bpi).map(v => v.rate);			 
+			                       }
+		                           }
+XHR.open("GET","https://api.coindesk.com/v1/bpi/currentprice.json");
+XHR.send();
+function getRate(id) {
+price.innerHTML = data[id];
+}
